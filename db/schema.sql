c56f1a540add62e223ddf0ec67f71d76a7d4cb53
timothydlee <timothy.d.lee@live.com>
2016-12-17 16:13:24 -0600

    updated schema to be correct

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ USE burgers_db;
 CREATE TABLE burgers (
 	id INTEGER(11) AUTO_INCREMENT NOT NULL,
 	burger_name VARCHAR(255) NOT NULL,
-	devoured BOOLEAN NOT NULL DEFAULT 'false',
-	date TIMESTAMP NOT NULL,
+	devoured BOOLEAN DEFAULT '0',
+	createdAt TIMESTAMP NOT NULL,
 	PRIMARY KEY(id)
 );
